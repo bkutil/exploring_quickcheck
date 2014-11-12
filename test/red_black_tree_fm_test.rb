bce6ec9033ad8cb56706f4707924cb350f0aacc6
Balazs Kutil <balazs@kutilovi.cz>
2014-11-12 09:32:32 +0100

    Add tests on rb trees using finite machine

--- test/red_black_tree_fm_test.rb
+++ test/red_black_tree_fm_test.rb
@@ -1,20 +1,58 @@
 require 'test_helper'
 require 'finite_machine'
+require 'test_helper'
 require_relative '../lib/red_black_tree'
 
 class RedBlackTreeFmTest < Minitest::Test
   def test_rb_tree
-    tree   = RedBlackTree.new
-    list   = []
-    transitions = []
+    m = TestMachine.new
+    1.upto(100) do |n|
+      m.restore!
+      n.times do
+        m.next(rand)
+      end
 
-    fm = FiniteMachine.define do
+      assert_equal m.tree.length, m.list.length, [m.history, m.tree.to_a, m.list].map(&:inspect)
+      assert_equal m.tree.keys.sort, m.list.sort, [m.history, m.tree.to_a, m.list].map(&:inspect)
+    end
+  end
+end
+
+# A finitime machine that records its state transitions
+class TestMachine
+  attr_accessor :state_machine
+
+  attr_accessor :history
+  attr_accessor :tree
+  attr_accessor :list
+
+  def method_missing(name, *args, &block)
+    if state_machine.respond_to?(name)
+      state_machine.send(name, *args, &block)
+    else
+      super
+    end
+  end
+
+  def restore!
+    @tree = RedBlackTree.new
+    @list = []
+    @history = []
+    @state_machine = build_state_machine
+  end
+
+  def build_state_machine
+    context = self
+
+    FiniteMachine.define do
       initial :empty
+
+      target context
 
       events {
         event :next, from: [:empty, :add, :remove] do
-           choice :add, if: -> (context, w) { w < 0.5 || list.empty? }
-           choice :remove, if: -> (context, w) { w >= 0.5 || !list.empty? }
+           choice :add,    if: -> (context, w) { w < 0.5 }
+           choice :remove, if: -> (context, w) { w >= 0.5 }
         end
       }
 
@@ -22,34 +60,22 @@ class RedBlackTreeFmTest < Minitest::Test
         on_transition(:add) do |evt|
           key = rand(1024), val = rand(1024)
 
-          list.push(val) unless list.include?(val)
-          tree[key] = val
+          context.list.push(key) unless context.list.include?(key)
+          context.tree[key] = val
         end
 
         on_transition(:remove) do |evt|
-          key = tree.keys.shuffle.first
+          key = context.tree.keys.shuffle.first
 
-          list.delete(tree[key])
-          tree.delete(key)
+          context.list.delete(key)
+          context.tree.delete(key)
         end
 
         on_transition do |evt|
-          transitions.push(evt.from)
+          context.history.push(evt.from)
         end
       }
     end
-
-    1.upto(100).each do |n|
-      fm.restore!(:empty)
-
-      list = []
-      tree = RedBlackTree.new
-      transitions = []
-
-      n.times do
-        fm.next(rand)
-        assert_equal list.length, tree.length, [transitions, tree, list].map(&:inspect)
-      end
-    end
   end
 end
+
